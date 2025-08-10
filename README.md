212b74d689e920fe3aefd2042b906c42364a225d
Rising_Date <1773261467@qq.com>
2025-08-10 16:13:03 +0800

    使用vite创建项目

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Experiment-Design-Font-End
-实验流程自动化前端代码
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    port: 8080, // 运行端口
+    open: true,  // 自动打开浏览器
+    proxy: {                 //设置代理，必须填
+      '/api': {              //设置拦截器  拦截器格式   斜杠+拦截器名字，名字可以自己定
+        target: "http://localhost:9090/",     //代理的目标地址
+        // target: "http://211.81.55.186:9090/",     //代理的目标地址
+        changeOrigin: true,              //是否设置同源，输入是的
+        pathRewrite: {                   //路径重写
+          '/api': ''                   //选择忽略拦截器里面的单词
+        }
+      }
+    }
+  }
+})
